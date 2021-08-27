07891d95a50e435b4fd532074fa1b15f8fd45b67
Vatsal99 <32352264+Vatsal99@users.noreply.github.com>
2021-08-27 21:51:58 +0530

    Desktop notifier code

---
+++ notifier.py
@@ -0,0 +1,11 @@
+# from plyer import notification
+
+# title= 'Sample notification!'
+# message= 'Hello word! '
+# notification.notify(title=title,
+#                       message= message,
+#                       app_icon= None,
+#                       timeout= 10,
+#                       toast=False)
+
+
